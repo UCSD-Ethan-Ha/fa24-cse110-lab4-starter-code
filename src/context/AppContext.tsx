d0c8db77c463939453b54db5d24f71e7d28df78b
Ethan Ha <ethan.ha@gmail.com>
2024-10-21 22:45:03 -0700

    Exercise 1 should be done

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -6,23 +6,32 @@ import { Expense } from "../types/types";
 interface AppContextType {
   expenses: Expense[];
   setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
+  //idk if it's supposed to return anything but if it is then replace "void" with w/e
+  addBudget: (expenses: Expense) => void;
 }
 
 const initialState: AppContextType = {
   expenses: [],
-  setExpenses: () => {},
+  setExpenses: () => { },
+  addBudget: () => [],
 };
 
 export const AppContext = createContext<AppContextType>(initialState);
 
+
+//im pretty confident abt everything for exercise 1 except for the AppProvider fnc with the const addBudget here
 export const AppProvider = (props: any) => {
   const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
+  const addBudget = (newExpense: Expense) => {
+    setExpenses((prevExpenses) => prevExpenses.concat(newExpense));
+  };
 
   return (
     <AppContext.Provider
       value={{
         expenses: expenses,
         setExpenses: setExpenses,
+        addBudget: addBudget,
       }}
     >
       {props.children}
