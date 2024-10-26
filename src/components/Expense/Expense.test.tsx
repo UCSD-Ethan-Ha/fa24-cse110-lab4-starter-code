049c29824958c88dd018ded7d87bc5dd4255c68d
Ethan Ha <ethan.ha@gmail.com>
2024-10-25 23:35:53 -0700

    test cases are up just need to debug

--- src/components/Expense/Expense.test.tsx
+++ src/components/Expense/Expense.test.tsx
@@ -1,15 +1,16 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import AddExpenseForm from './AddExpenseForm';
+import ExpenseList from "./ExpenseList";
 import { AppContext } from '../../context/AppContext';
 
 test("create an expense", () => {
     render(
-        <AddExpenseForm />
+        <ExpenseList />
     );
 
-    const nameInput = screen.getByPlaceholderText("");
-    const costInput = screen.getByPlaceholderText("0");
-    const submitButton = screen.getByRole("button", { name: /Save/});
+    const nameInput = screen.getByLabelText(/Name/i);
+    const costInput = screen.getByLabelText(/Cost/i);
+    const submitButton = screen.getByText("Save");
 
     fireEvent.change(nameInput, { target: { value: "Apples" } });
     fireEvent.change(costInput, { target: { value: "100" } });
@@ -19,37 +20,57 @@ test("create an expense", () => {
     const addedExpense = screen.getByText("Apples");
     expect(addedExpense).toBeInTheDocument();
 
-    const addedCost = screen.getByText("100"); 
+    const addedCost = screen.getByText("100");
     expect(addedCost).toBeInTheDocument();
 
-
+    const totalSpent = screen.getByTestId('Spent so far:');
+    const remaining = screen.getByTestId('Remaining');
+    
+    expect(totalSpent).toBe(100); 
+    expect(remaining).toBe(900); 
 
 });
 
-test("create an expense", () => {
+test("Delete an expense", () => {
     render(
-        <AddExpenseForm />
+        <ExpenseList />
     );
+    const deleteButton = screen.getAllByText("x")[0];
+    fireEvent.click(deleteButton);
 
-    const nameInput = screen.getByLabelText(/Name/);
-    const costInput = screen.getByLabelText(/Cost/);
-    const submitButton = screen.getByRole("button", { name: /Save/i});
+    expect(screen.queryByText(/Apples/i)).not.toBeInTheDocument();
 
-    fireEvent.change(nameInput, { target: { value: "Apples" } });
-    fireEvent.change(costInput, { target: { value: "100" } });
-    fireEvent.click(submitButton);
-
-    screen.debug();
-
-    const addedExpense = screen.getByText("Apples");
-    expect(addedExpense).toBeInTheDocument();
-
-    const addedCost = screen.getByText("100"); 
-    expect(addedCost).toBeInTheDocument();
-
-
+    const totalSpent = screen.getByTestId('Spent so far:');
+    const remaining = screen.getByTestId('Remaining');
+    expect(totalSpent).toHaveTextContent('$0');
+    expect(remaining).toHaveTextContent('$1000');
 
 });
 
+test("Check Budget Balance and eqn", () => {
+    render(
+        <ExpenseList />
+    );
 
+    const nameInput = screen.getByLabelText(/name/i);
+    const costInput = screen.getByLabelText(/cost/i);
+    const submitButton = screen.getByText("Save");
+  
+    fireEvent.change(nameInput, { target: { value: 'Ingredients' } });
+    fireEvent.change(costInput, { target: { value: '300' } });
+    fireEvent.click(submitButton);
+  
+    fireEvent.change(nameInput, { target: { value: 'Utilities' } });
+    fireEvent.change(costInput, { target: { value: '100' } });
+    fireEvent.click(submitButton);
+  
+    // Verify the budget balance equation
+    const budget = 1000; 
+    //for some reason we need the all the code from textContext on for this to work. Mainly bc we can't add remaining + totalSpent since they're HTML elements I believe
+    const totalSpent = parseFloat(screen.getByTestId('Spent so far:').textContent!.replace('$', ''));
+    const remaining = parseFloat(screen.getByTestId('Remaining').textContent!.replace('$', ''));
+  
+    expect(budget).toBeCloseTo(remaining + totalSpent, 2);
+
+});
 
